511ef9c7582642f1d56551f75fd3293475086ba2
Bipanrai10 <160600523+Bipanrai10@users.noreply.github.com>
2024-02-20 17:33:37 +0545

    oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class  odd_even{
+public class  oddeven{
     public static void main(String[]args){
     Scanner read=new Scanner(System.in);
     System.out.print("Enter a number");
